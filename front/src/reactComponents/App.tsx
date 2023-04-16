9003dbea3820dd0cf227c6aff5ef2d6b3b453f47
adam-vonbismarck <adam_von_bismarck@brown.edu>
2023-04-16 14:51:57 -0400

    Added a large number of web assets, including fonts logos and css to add it to the homepage.

--- front/src/reactComponents/App.tsx
+++ front/src/reactComponents/App.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "../styling/App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <video autoPlay muted loop id="backgroundVideo">
+        <source
+          src="/assets/video/CS32-background-video.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div className="menu">
+        <div className="logo">
+          <img src="/assets/logos/logo-light.svg" alt="Logo" />
+        </div>
+        <div className="menu-items">
+          <button>About</button>
+          <button>Register</button>
+          <button>Sign In</button>
+        </div>
+        <p className="description">
+          Short paragraph explaining what the site does
+        </p>
+        <div className="social-icons">
+          <img src="/assets/icons/instagram.svg" alt="Instagram" />
+          <img src="/assets/icons/twitter.svg" alt="Twitter" />
+          <img src="/assets/icons/facebook.svg" alt="Facebook" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
