106147739846fffd4504780d95bada2d9d6b5500
adam-vonbismarck <adam_von_bismarck@brown.edu>
2023-04-16 13:33:19 -0400

    Intial front end setup

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styling/index.css";
-import { getMainOverlay, getSearchedOverlay } from "./overlays";
 import App from "./reactComponents/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App
-      getMainOverlayResponse={getMainOverlay}
-      getSearchOverlayResponse={getSearchedOverlay}
-    />
-  </React.StrictMode>
+  <App />
 );
--- front/src/private/api.tsx
+++ front/src/private/api.tsx
@@ -1,6 +1,4 @@
 let API_KEY: string;
 
-API_KEY =
-  "pk.eyJ1IjoiYWRhbS12b25iaXNtYXJjayIsImEiOiJjbGZ6cTg3cW0wMGNnM2NvYXZxbWZvbHloIn0.6HHQWSK4mDq3AJuEEWDoJw";
-
+API_KEY = "";
 export { API_KEY };
