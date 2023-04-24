7b5c9f8b7f30b17c865de84b610e4b9b508ea200
adam-vonbismarck <adam_von_bismarck@brown.edu>
2023-04-23 20:27:43 -0400

    parralax test

--- front/src/reactComponents/pages/workoutDisplay.tsx
+++ front/src/reactComponents/pages/workoutDisplay.tsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 import "../../styling/App.css";
 import LoggedInMenu from "../elements/loggedInMenu";
 import WorkoutCalendar from "../elements/calendar";
+import { Parallax } from "react-parallax";
 
 function WorkoutDisplay() {
   return (
     <div className={"content-container"}>
       <div className="menu-container">
-        <LoggedInMenu description="The exercise program generator for your fitness journey." />
+        <LoggedInMenu description="Click on a day for more detials and to record your data." />
       </div>
       <div className="content-wrapper">
+        <h1>Workout Plan</h1>
         <div className="calendar-container">
           <WorkoutCalendar />
         </div>
