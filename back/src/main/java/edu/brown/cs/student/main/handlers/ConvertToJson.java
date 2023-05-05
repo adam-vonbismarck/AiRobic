c0dda75682c2c16fd5d68e58996156f94341aaf3
adam-vonbismarck <adam_von_bismarck@brown.edu>
2023-05-05 12:19:25 -0400

    converter fix

--- back/src/main/java/edu/brown/cs/student/main/handlers/ConvertToJson.java
+++ back/src/main/java/edu/brown/cs/student/main/handlers/ConvertToJson.java
@@ -1,3 +1,40 @@
+//package edu.brown.cs.student.main.handlers;
+//
+//import edu.brown.cs.student.main.models.formattypes.Day;
+//import edu.brown.cs.student.main.models.formattypes.Schedule;
+//import edu.brown.cs.student.main.models.formattypes.Week;
+//import edu.brown.cs.student.main.models.markov.Emission;
+//
+//import java.time.format.DateTimeFormatter;
+//
+//public class ConvertToJson {
+//
+//    public static String convert(Schedule schedule) {
+//        String jsonString = "{";
+//        for (Week week : schedule.weeks()) {
+//            for (Day day : week.days()) {
+//                assert(!day.getDate().isEmpty());
+//                jsonString += "\"" +
+//                        (day.getDate().get().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"))) +
+//                        "\" : {";
+//                int workoutCounter = 0;
+//                if (day.getEmissions().size() == 0) {
+//                    jsonString += " }";
+//                }
+//                for (Emission emission : day.getEmissions()) {
+//                    workoutCounter++;
+//                    jsonString += "\"" + workoutCounter + "\" : { \"workout\" : \"" + emission.getWorkout() +
+//                            "\", \"duration\" : \"" + emission.getTime() + "\" }" +
+//                            ((day.getEmissions().indexOf(emission) == day.getEmissions().size() - 1) ? " }" : ", ");
+//                }
+//                jsonString += ", ";
+//            }
+//        }
+//        return jsonString.substring(0, jsonString.length() - 2);
+//    }
+//
+//}
+
 package edu.brown.cs.student.main.handlers;
 
 import edu.brown.cs.student.main.models.formattypes.Day;
@@ -10,27 +47,42 @@ import java.time.format.DateTimeFormatter;
 public class ConvertToJson {
 
     public static String convert(Schedule schedule) {
-        String jsonString = "{";
+        StringBuilder jsonString = new StringBuilder("{");
+
         for (Week week : schedule.weeks()) {
             for (Day day : week.days()) {
                 assert(!day.getDate().isEmpty());
-                jsonString += "\"" +
-                        (day.getDate().get().format(DateTimeFormatter.ofPattern("MM-dd-yyyy"))) +
-                        "\" : {";
-                int workoutCounter = 0;
-                if (day.getEmissions().size() == 0) {
-                    jsonString += " }";
-                }
+
+                jsonString.append("\"")
+                        .append(day.getDate().get().format(DateTimeFormatter.ofPattern("MM-dd-yyyy")))
+                        .append("\" : {");
+
+                int emissionCounter = 0;
                 for (Emission emission : day.getEmissions()) {
-                    workoutCounter++;
-                    jsonString += "\"" + workoutCounter + "\" : { \"workout\" : \"" + emission.getWorkout() +
-                            "\", \"duration\" : \"" + emission.getTime() + "\" }" +
-                            ((day.getEmissions().indexOf(emission) == day.getEmissions().size() - 1) ? " }" : ", ");
+                    emissionCounter++;
+                    jsonString.append("\"")
+                            .append(emissionCounter)
+                            .append("\" : { \"workout\" : \"")
+                            .append(emission.getWorkout())
+                            .append("\", \"duration\" : \"")
+                            .append(emission.getTime())
+                            .append("\" }");
+
+                    if (emissionCounter < day.getEmissions().size()) {
+                        jsonString.append(",");
+                    }
                 }
-                jsonString += ", ";
+
+                jsonString.append("}");
+
+                if (day != week.days().get(week.days().size() - 1)) {
+                    jsonString.append(",");
+                }
             }
         }
-        return jsonString.substring(0, jsonString.length() - 2);
-    }
 
+        jsonString.append("}");
+
+        return jsonString.toString();
+    }
 }
